b39ca869a1a0717dc5b350bea4721cda3dd4f87b
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-16 04:16:51 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,6 +11,10 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
+    names = []
+    self.artists.each do |artist|
+      names << artist.name
+    
     self.artists.name
   end
 end
